58be777a09d7111d59adb2d9e4ce2d7a8fa10bbd
jyothir <jyothirts@gmail.com>
2017-11-15 22:48:05 +0530

    few changes

--- app_rest_layer/src/main/java/org/java/jyothir/app/rest/host/LoginRestService.java
+++ app_rest_layer/src/main/java/org/java/jyothir/app/rest/host/LoginRestService.java
@@ -1,8 +1,17 @@
 package org.java.jyothir.app.rest.host;
 
+import com.sun.org.apache.regexp.internal.RE;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+
+import javax.print.attribute.standard.Media;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.io.*;
+import java.util.List;
 
 @Path("/login")
 public class LoginRestService {
@@ -57,6 +66,113 @@ public class LoginRestService {
                         + " , lastName " + lastName + "}";
         return Response.status(Response.Status.OK).entity(result).build();
     }
+
+    /** @QueryParam
+     * http://localhost:8080/myApp/login/query2?firstName=j&lastName=s&sortBy=firstname&sortBy=id */
+    @GET
+    @Path("/query2")
+    public Response queryByName2(@Context UriInfo uriInfo
+    ){
+        String firstName = uriInfo.getQueryParameters().getFirst("firstName");
+        String lastName = uriInfo.getQueryParameters().getFirst("lastName");
+        List<String> sortBy = uriInfo.getQueryParameters().get("sortBy");
+        String result = "queryByName2 { firstName : " + firstName
+                + " , lastName " + lastName + "} sortBy => " + sortBy.toString();
+
+        return Response.status(Response.Status.OK).entity(result).build();
+    }
+
+    /**
+     * Get an html page for registering new user
+     * http://localhost:8080/myApp/login/queryHtml
+     */
+    @GET
+    @Path("/queryHtml")
+    @Produces(MediaType.TEXT_HTML)
+    public Response getQueryAsHtml(){
+        StringBuilder html = new StringBuilder();
+        html.append("<html><body>")
+                .append("<form action=\"user/register\" method=\"post\">")
+                .append("<p>FirstName : <input type=\"text\" name=\"firstName\"></p>")
+                .append("<p>LastName : <input type=\"text\" name=\"lastName\"></p>")
+                .append("<input type=\"submit\" value=\"Register\">")
+                .append("</form></body></html>");
+        return Response.status(Response.Status.OK).entity(html.toString()).build();
+    }
+
+    /**
+     * @FormParam example => data will be posted to
+     * http://localhost:8080/myApp/login/user/register
+     */
+    @POST // insert
+    @Path("/user/register")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response register(@FormParam("firstName") String firstName,
+                             @FormParam("lastName") String lastName
+                             ){
+        String result = "registered new user : " + firstName + " " + lastName;
+        return Response.status(Response.Status.OK).entity(result).build();
+    }
+
+
+/** file upload
+ * http://localhost:8080/myApp/login/uploadHtml
+ */
+    @GET
+    @Path("/uploadHtml")
+    @Produces(MediaType.TEXT_HTML)
+    public Response uploadFileHtml(){
+        StringBuilder html = new StringBuilder();
+        html.append("<html><body>")
+                .append("<form action=\"user/file/upload\" method=\"post\" enctype=\"multipart/form-data\">")
+                .append("<p>Add file to upload : <input type=\"file\" name=\"file\" size=\"50\" ></p>")
+                .append("<input type=\"submit\" value=\"Upload\">")
+                .append("</form></body></html>");
+        return Response.status(Response.Status.OK).entity(html.toString()).build();
+    }
+
+
+    /**
+     * http://localhost:8080/myApp/login/user/file/upload
+     */
+    @POST
+    @Path("/user/file/upload")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response uploadFile(@FormDataParam("file") InputStream uploadedInputStream,
+                               @FormDataParam("file")FormDataContentDisposition fileMetaData) {
+        String destination = "/usr/local/uploaded" + fileMetaData.getFileName();
+        saveFile(uploadedInputStream, destination);
+        String output = "File uploaded : " + destination;
+        return Response.status(Response.Status.OK).entity(output).build();
+    }
+
+    private void saveFile(InputStream uploadedInputStream ,
+                          String saveDestination) {
+        try(OutputStream out = new FileOutputStream(new File(saveDestination));){
+            int read = 0;
+            byte[] bytes = new byte[1024];
+            while((read = uploadedInputStream.read(bytes)) != -1) {
+                out.write(bytes,0,read);
+            }
+        }catch (FileNotFoundException e1){
+            e1.printStackTrace();
+        }catch (IOException e1){
+            e1.printStackTrace();
+        }
+    }
+
+    /** http://localhost:8080/myApp/login/getEmployee */
+    @GET
+    @Path("/getEmployee")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getEmployee(){
+        return Response.status(Response.Status.OK).entity("" +
+                "{ \"name\":\"John\",\"age\":31, \"city\":\"New York\"}").build();
+    }
+
+
+
 
 
 
